c5346225fb578ffa0ca0f8e4f4aac68d92274fe9
Runbin Zhou <runbin1000@gmail.com>
2025-01-17 20:55:44 -0500

    added boilerplate code for website

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  )
+}
+
+console.log(React);
+
+export default App
+
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>Navbar</h1>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
+
+console.log(React);
+
+export default Navbar;
